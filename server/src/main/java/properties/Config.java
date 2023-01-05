3ca8d37b36241bb3afafc4f7f26ddaf8834cd1e5
NamVH <namvh194809@sis.hust.edu.vn>
2023-01-05 08:02:57 -0800

    feat: add properties and config

---
+++ server/src/main/java/properties/Config.java
@@ -0,0 +1,13 @@
+package properties;
+
+public class Config {
+    public static int SESSION_TIME_OUT = 300; //in second
+
+    //172.16.194.120
+    public static String SERVER_IP = "172.16.194.120";
+
+    public static long GAME_LENGTH = 60;
+    public static int MINIMUM_NUMBER_OF_PLAYERS = 2;
+    public static int NUMBER_OF_FRUITS = 5;
+    public static int NUMBER_OF_ENEMIES = 15;
+}
